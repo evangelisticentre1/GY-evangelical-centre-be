426b31b98cd3da58130fd81ff7f80e48e1769aee
uwishema11 <mpanoc6@gmail.com>
2023-11-11 18:36:35 +0200

    set up testing

---
+++ src/tests/test.js
@@ -0,0 +1,18 @@
+
+
+import chaiHttp  from 'chai-http';
+import chai from 'chai';
+import app from '../app.js'
+
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe('Testing', ()=>{
+    describe('Math',()=>{
+        it('should add two numbers', ()=>{
+            const result=2+3;
+            expect(result).to.equal(5)
+        })
+    })
+})
